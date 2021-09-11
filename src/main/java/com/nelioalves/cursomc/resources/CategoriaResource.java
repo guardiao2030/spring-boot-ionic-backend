efa7f3a8e5f7c2ccbffdedafb353566824d60e43
Cleyton Caldas <cleyton.pcaldas@gmail.com>
2021-09-11 01:32:13 -0300

    Inserindo uma nova Categoria com Post

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -3,10 +3,14 @@ package com.nelioalves.cursomc.resources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import java.util.List;
+import java.net.URI;
 import java.util.ArrayList;
 
 import com.nelioalves.cursomc.domain.CategoriaDomain;
@@ -27,5 +31,15 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(obj);
 		
 	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody CategoriaDomain obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+	}
+	
 	
 }
--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -22,4 +22,9 @@ public class CategoriaService {
 				"Objeto não encontrado! id:"+ id + ", Tipo : "+ CategoriaDomain.class.getName()));
 	}
 	
+	public CategoriaDomain insert(CategoriaDomain obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
 }
