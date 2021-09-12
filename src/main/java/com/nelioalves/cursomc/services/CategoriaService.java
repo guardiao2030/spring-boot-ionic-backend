e37ecb197f55314dc6794629b5c1edbdfa5029e8
CLEYTON CALDAS <CLEYTON.PCALDAS@GMAIL.COM>
2021-09-12 12:30:20 -0300

    Deletando uma categoria com Delete

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -3,6 +3,7 @@ package com.nelioalves.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.cursomc.domain.CategoriaDomain;
@@ -32,5 +33,18 @@ public class CategoriaService {
 		find(obj.getId());
 		return repo.save(obj);
 	}
+
+	public void delete(Integer id) {
+		// TODO Auto-generated method stub
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
+		}
+	}
+	
+	
 	
 }
---
+++ src/main/java/com/nelioalves/cursomc/services/exceptions/DataIntegrityException.java
@@ -0,0 +1,22 @@
+package com.nelioalves.cursomc.services.exceptions;
+
+public class DataIntegrityException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DataIntegrityException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DataIntegrityException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	
+}
