99763c6d0e8833f5a96f72699ae707296e831442
Cleyton Caldas <67482635+guardiao2030@users.noreply.github.com>
2021-09-12 23:21:18 -0300

    Revert "atualizado arquivos em casa teste"
    This reverts commit 7825bcd425be9c1d3fbf314a3921faad5f4c024a.

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -10,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.List;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.net.URI;
+import java.util.ArrayList;
+
 import com.nelioalves.cursomc.domain.CategoriaDomain;
 import com.nelioalves.cursomc.dto.CategoriaDTO;
 import com.nelioalves.cursomc.services.CategoriaService;
